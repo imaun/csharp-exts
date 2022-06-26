5cd707e9bb157dbe191b5dca09625b02fda56470
imun <imun22@gmail.com>
2022-06-26 16:39:58 +0430

    Add missing files

--- src/Roonia.Extensions/Common/BooleanExts.cs
+++ src/Roonia.Extensions/Common/BooleanExts.cs
@@ -1,7 +1,20 @@
-namespace Roonia.Extensions.Common
+namespace Roonia.Extensions
 {
-    public class BooleanExts
+    public static class BooleanExts
     {
         
+        public static bool WhenTrue(this bool condition, Action action) {
+            if(condition)
+                action();
+
+            return condition;
+        }
+
+        public static bool Else(this bool condition, Action action) {
+            if(!condition)
+                action();
+            
+            return condition;
+        }
     }
 }
--- src/Roonia.Extensions/Common/HashingExts.cs
+++ src/Roonia.Extensions/Common/HashingExts.cs
@@ -5,8 +5,15 @@ namespace Roonia.Extensions
     public static class HashingExts
     {
         
+        /// <summary>
+        /// Hash input string with an optional salt.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
         public static string HashString(this string input, string salt = null) {
-            
+            if(salt.IsNullOrEmpty()) input = $"{input}{salt}";
+
             using (var md5Hash = MD5.Create())
             {
                 byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
